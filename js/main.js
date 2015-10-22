60ec24120f4852805f7cda1b30f592e5d07a61cd
JordanWilson <mrwizard6933@gmail.com>
2015-10-21 23:40:45 -0400

    style changes

---
+++ js/main.js
@@ -0,0 +1,172 @@
+
+$(document).ready(function () {
+
+    var canvas = document.getElementById("canvas"),
+    ctx = canvas.getContext("2d");
+		
+		
+		
+		
+		
+		
+		
+	// Obtain a reference to the canvas element
+	// using its id.
+	//htmlCanvas = document.getElementById('c'),
+   // Obtain a graphics context on the
+   // canvas element for drawing.
+   //context = htmlCanvas.getContext('2d');
+
+	// Start listening to resize events and
+	// draw canvas.
+	initialize();
+
+	function initialize() {
+	// Register an event listener to
+	// call the resizeCanvas() function each time
+	// the window is resized.
+		window.addEventListener('resize', resizeCanvas, false);
+	// Draw canvas border for the first time.
+		resizeCanvas();
+	}
+	// Display custom canvas.
+	// In this case it's a blue, 5 pixel border that
+	// resizes along with the browser window.
+	function redraw() {
+		//ctx.strokeStyle = 'blue';
+		ctx.lineWidth = '.1';
+		ctx.strokeRect(0, 0, window.innerWidth, window.innerHeight);
+	}
+	// Runs each time the DOM window resize event fires.
+	// Resets the canvas dimensions to match window,
+	// then draws the new borders accordingly.
+	function resizeCanvas() {
+		canvas.width = (window.innerWidth) - 150;
+		canvas.height = (window.innerHeight) - 100;
+		redraw();
+	}
+			
+		
+		
+		
+		
+		
+		//var ctx = (a canvas context);
+		//ctx.canvas.width  = window.innerWidth;
+		// ctx.canvas.height = window.innerHeight;
+  
+		//canvas.width = window.innerWidth;
+		//canvas.height = window.innerHeight;
+        //canvas.width = 300;
+        //canvas.height = 300;
+
+        var x = 150, //player 1 positioning and speed
+            y = 150,
+            velY = 0,
+            velX = 0,
+            speed = 3,
+            friction = 0.5, //0.98
+            keys = [];
+
+        function update() { //player movement
+
+            if (keys[38]) {
+                if (velY > -speed) {
+                    velY--;
+                }
+            }
+
+            if (keys[40]) {
+                if (velY < speed) {
+                    velY++;
+                }
+            }
+            if (keys[39]) {
+                if (velX < speed) {
+                    velX++;
+                }
+            }
+            if (keys[37]) {
+                if (velX > -speed) {
+                    velX--;
+                }
+            }
+
+            velY *= friction; //friction and positioning
+            y += velY;
+            velX *= friction;
+            x += velX;
+
+            //if (x >= 285) { // colision with game boarders
+                //x = 285;
+            //} else if (x <= 15) {
+                //x = 15;
+            //}
+			
+			
+			if (x >= canvas.width - 15) { // colision with game boarders x-axis
+                x = canvas.width - 15;
+            } else if (x <= 15) {
+                x = 15;
+            }
+			
+			if (y > canvas.height - 15) { // colision with game boarders y-axis
+                y = canvas.height - 15;
+            } else if (y <= 15) {
+                y = 15;
+            }
+			
+			
+			
+
+            //if (y > 285) {
+                //y = 285;
+            //} else if (y <= 15) {
+                //y = 15;
+            //}
+
+
+
+            if (x <= 100 && y >= 90 && y <= 100 && x >= 90) { // player colision with ai
+                x = 80;
+                y = 80;
+            } //else if (x <= 35) {
+              // x = 35;
+            //}
+
+            //if (y <= 100) {
+            //    y = 100;
+            //} else if (y <= 15) {
+            //    y = 15;
+            //}
+
+
+
+            
+            ctx.beginPath();
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            ctx.fillStyle = "blue";
+            ctx.arc(x, y, 15, 0, Math.PI * 2); // draw the player
+            ctx.fill();
+            ctx.closePath();
+
+            
+            ctx.beginPath();
+            ctx.fillStyle = "black";
+            ctx.arc(100, 100, 15, 0, Math.PI * 2); // draw the ai
+            ctx.fill();
+            ctx.closePath();
+
+
+            setTimeout(update, 10); //refresh the screen
+        }
+
+        update();
+
+        document.body.addEventListener("keydown", function (e) {
+            keys[e.keyCode] = true;
+        });
+        document.body.addEventListener("keyup", function (e) {
+            keys[e.keyCode] = false;
+        });
+    });
